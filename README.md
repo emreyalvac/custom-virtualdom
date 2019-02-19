c90088dd149b9bc3471ef8a1250f9171d40150ef
emreyalvac <emre.stress@gmail.com>
2019-02-19 11:04:46 +0300

    initialize

---
+++ README.md
@@ -0,0 +1,3 @@
+Custom Virtual DOM
+
+For build -> 'npm run build'
---
+++ app.js
@@ -0,0 +1,49 @@
+import h from './my-hyperscript.js'
+import Component from './component'
+import { diff } from './v-dom.js';
+
+const getRandomItemFromArray = (list) => {
+  return list[
+    Math.round(Math.random() * (list.length - 1))
+  ];
+};
+
+class App extends Component {
+  render() {
+    return h('div', { class: 'app' },
+      h('h1', null, 'Custom virtual DOM'),
+      h(People)
+    )
+  }
+};
+
+class People extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      list: [
+        '1', '2'
+      ]
+    }
+
+    this.timer = setInterval(_ => {
+      this.setState({
+        list: [...this.state.list, getRandomItemFromArray(this.state.list)]
+      })
+    }, 1000)
+  }
+
+  render(props, state) {
+    return h(
+      'ul', null,
+      ...state.list.map(item => h('li', null, item))
+    )
+  }
+}
+
+const render = (vnode, parent) => {
+  diff(undefined, vnode, parent)
+}
+
+render(h(App), document.getElementById('root'))
